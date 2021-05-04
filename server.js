20fccf26b5db0786b4788796e5dd96cc337e0dd5
Camilo Caroso <finished92for@gmail.com>
2021-05-04 02:11:00 -0400

    sef

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 // const port = parseInt(process.env.PORT, 10) || 8080
 const port = 80
@@ -10,8 +11,11 @@ const handle = app.getRequestHandler()
 
 const testRoutes = require('./routes/test')
 
+
+
 app.prepare().then(() => {
   const server = express()
+  server.use(morgan('dev'))
   server.use(bodyParser.urlencoded({ extended: true }))
   server.use(bodyParser.json())
 
